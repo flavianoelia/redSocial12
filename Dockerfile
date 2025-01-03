ad2656296d0729cbe17f56d5271455e582f766de
flavianoelia <flavianoelia79@gmail.com>
2025-01-03 02:14:54 -0300

    Configuración de Docker

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM node:20-alpine
 WORKDIR /app
-COPY . .
+COPY package*.json ./
 RUN npm install
+COPY . .
 EXPOSE 3000
 CMD ["sh", "-c", "sleep 10 && node sync.js && npm start"]
--- config/config.js
+++ config/config.js
@@ -1,3 +1,4 @@
+/*
 const parameters = { // Parametros de configuración de la base de datos
     username: "root", // Nombre de usuario de la base de datos
     password: "1234", // Contraseña de la base de datos
@@ -5,5 +6,14 @@ const parameters = { // Parametros de configuración de la base de datos
     host: "localhost", // Host de la base de datos
     dialect: "mysql", // Dialecto de la base de datos
 };
+*/
 
+const parameters = {
+    username: process.env.DB_USER || "root",
+    password: process.env.DB_PASSWORD || "1234",
+    database: process.env.DB_NAME || "db_final",
+    host: process.env.DB_HOST || "db",
+    port: process.env.DB_PORT || "3306",
+    dialect: "mysql",
+};
 module.exports = parameters;

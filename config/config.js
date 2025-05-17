a960750b156ac5962bc6d31584fbb6bf1c49d590
Flavia Noelia Perin <flavianoelia79@gmail.com>
2025-05-17 15:12:50 -0300

    Update DB config for MySQL and WAMP environment

--- config/config.js
+++ config/config.js
@@ -1,18 +1,9 @@
-/*
-const parameters = { // Parametros de configuración de la base de datos
-    username: "root", // Nombre de usuario de la base de datos
-    password: "1234", // Contraseña de la base de datos
-    database: "db_final", // Nombre de la base de datos
-    host: "localhost", // Host de la base de datos
-    dialect: "mysql", // Dialecto de la base de datos
-};
-*/
-
+require('dotenv').config(); // Carga .env
 const parameters = {
     username: process.env.DB_USER || "root",
     password: process.env.DB_PASSWORD || "1234",
     database: process.env.DB_NAME || "db_final",
-    host: process.env.DB_HOST || "db",
+    host: process.env.DB_HOST || "localhost",
     port: process.env.DB_PORT || "3306",
     dialect: "mysql",
 };

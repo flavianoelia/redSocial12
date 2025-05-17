c5629c85ae8fee0a3318b3a055f5abb28f4b1cfd
Flavia Noelia Perin <flavianoelia79@gmail.com>
2025-05-17 15:02:53 -0300

    Improve token extraction logic in auth middleware

--- middlewares/authmiddleware.js
+++ middlewares/authmiddleware.js
@@ -1,24 +1,27 @@
 const jwt = require("jsonwebtoken");
 
 const auth = (req, res, next) => {
-    // const token = req.header("Authorization")?.replace(/"/g, "").trim(); // Quita comillas y espacios
-    const token = req.header("Authorization"); // sin limpiar comillas ni espacios
-    // Verificar si existe el token
-    if (!token) {
+    const authHeader = req.header("Authorization") 
+    if (!authHeader) {
         return res.status(401).send({ message: "No hay token" });
     }
-    // decodificarlo
+    // Extrae el token quitando el prefijo "Bearer " (si existe)
+    const tokenExtracted = authHeader.startsWith ("Bearer")
+        ? authHeader.replace("Bearer ", "").trim()
+        : authHeader.trim();
+    // Verifica si el token es valido y lo decodifica
+    
     try {
-        const verified = jwt.verify(token, process.env.JWT_SECRET);
+        const verified = jwt.verify(tokenExtracted, process.env.JWT_SECRET);
         req.user = verified; // Guarda los datos decodificado
         next();
     } catch (error) {
         res.status(400).send({
             message: "token no valido",
             info: error.message
-        })
+        });
     }
 
-}
+};
 
 module.exports = auth;

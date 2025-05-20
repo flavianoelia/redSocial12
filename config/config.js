1998236963c64114860e7694e27deaa945aab8dd
Flavia Noelia Perin <flavianoelia79@gmail.com>
2025-05-20 01:47:12 -0300

    Update database config to use environment variables and MySQL defaults

--- config/config.js
+++ config/config.js
@@ -1,10 +1,20 @@
 require('dotenv').config(); // Carga .env
-const parameters = {
-    username: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "1234",
-    database: process.env.DB_NAME || "db_final",
-    host: process.env.DB_HOST || "localhost",
-    port: process.env.DB_PORT || "3306",
-    dialect: "mysql",
+
+module.exports = {
+    development: {
+        username: process.env.DB_USER || "root",
+        password: process.env.DB_PASSWORD || "1234",
+        database: process.env.DB_NAME || "db_final",
+        host: process.env.DB_HOST || "localhost",
+        port: process.env.DB_PORT || "3306",
+        dialect: "mysql"
+    },
+    production: {
+        username: process.env.DB_USER || "root",
+        password: process.env.DB_PASSWORD || "1234",
+        database: process.env.DB_NAME || "db_final",
+        host: process.env.DB_HOST || "localhost",
+        port: process.env.DB_PORT || "3306",
+        dialect: "mysql"
+    }
 };
-module.exports = parameters;
